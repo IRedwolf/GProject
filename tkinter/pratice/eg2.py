96ec5e9e01592c22898bc5e28a6d56cc02cc0bce
TingFeng <2078449839@qq.com>
2019-03-04 19:15:47 +0800

    tkinter_learning practice

---
+++ tkinter/pratice/eg2.py
@@ -0,0 +1,33 @@
+# Entry 输入控件 ；单行
+# Text 文本控件；多行显示
+# insert point 粘贴字符串到指针处
+# insert end 粘贴指针到末尾
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title('my window')
+window.geometry('200x200')
+
+e = tk.Entry(window,show=None)
+e.pack()
+
+def insert_point():
+    var = e.get()
+    t.insert('insert',var)
+
+def insert_end():
+    var=e.get()
+    t.insert('1.1', var)
+
+b1 = tk.Button(window,text='insert point',width=15,height=2,
+               command=insert_point)
+b1.pack()
+b2 = tk.Button(window,text='insert_end',
+               command=insert_end)
+b2.pack()
+
+t = tk.Text(window, height=2)
+t.pack()
+
+window.mainloop()
